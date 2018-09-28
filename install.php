7f04d366a3f7a5484a4dd39e06ceb31d214745e7
Rahel Ahmed <RahelA@transportinnovation.co.uk>
2018-09-28 11:27:27 +0100

    even more OOP seperation -- love OOP yay or nay?

---
+++ install.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ *	@author: rachow
+ *	@copyright: 2018
+ *	@package: Installer
+ */
+
+require_once __DIR__ . '/installer.php';
+
+
+/*
+ * Usage: php installer.php --laravel
+*/
+
+$commands = @$argv;
+$options = [];
+$show_help = false;
+
+foreach( $commands as $idx => $opt )
+{
+	if( $idx == 0 ) continue;
+	if( $opt == '--help' ){
+		$show_help = true;
+		break;
+	}
+	$options[] = strtolower(trim(str_replace( '--', '' , $opt )));
+}
+
+$installer = new Installer();
+$cnt_packages = count($options);
+
+if( $cnt_packages > 0 )
+{
+	for($j=0; $j <= $cnt_packages; $j++)
+	{
+		$installer->install_package($options[$j]);
+	}
+}
+
+/* now install all the packages */
+$installer->run();
+exit;
+
+
